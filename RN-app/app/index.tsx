a02cbd0021a7930cf5082bd6dbaaa86c0a822ed3
Seojun Yoo <honeybeei@Seojuns-MacBook-Pro.local>
2025-08-19 12:01:51 +0200

    ✨ Add: huh? gluestack-ui works.... wth?

--- RN-app/app/index.tsx
+++ RN-app/app/index.tsx
@@ -1,20 +1,13 @@
+import { Spinner } from "@/components/ui/spinner";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex flex-col flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Hello world!
       </Text>
+        <Spinner />
     </View>
-    // <View
-    //   style={{
-    //     flex: 1,
-    //     justifyContent: "center",
-    //     alignItems: "center",
-    //   }}
-    // >
-    //   <Text>Edit app/index.tsx to edit this screen.</Text>
-    // </View>
   );
 }
