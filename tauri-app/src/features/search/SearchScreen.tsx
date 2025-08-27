db6b5a7aceb9ff275b576211a0226f7740a84905
Seojun Yoo <honeybeei@Seojuns-MacBook-Pro.local>
2025-08-27 11:47:51 +0200

    🧹 Clean: removed tauri project.

--- tauri-app/src/features/search/SearchScreen.tsx
+++
@@ -1,7 +0,0 @@
-export default function SearchScreen() {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>Search Screen</h1>
-    </div>
-  );
-}
--- tauri-app/src/features/settings/SettingsScreen.tsx
+++
@@ -1,7 +0,0 @@
-export default function SettingsScreen() {
-  return (
-    <div className="flex flex-col items-center justify-center">
-      <h1>Settings Screen</h1>
-    </div>
-  );
-}
